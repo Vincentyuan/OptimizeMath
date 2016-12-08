f3b456335131213352e2f389e272117bfb5a6a53
vincent <526730329@qq.com>
2016-12-08 09:17:51 +0100

    Initial Commit

---
+++ OptimizeMath/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OptimizeMath
+//
+//  Created by 袁萍 on 2016/12/8.
+//  Copyright © 2016年 袁萍. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
