fe744020c56ea9db7911056c979da58ec6c3a434
zhangxulong0218 <zhangxulong0218@sina.com>
2017-01-07 18:20:34 +0100

    Update Masterbinary.c

--- OptimizeMath/Masterbinary.c
+++ OptimizeMath/Masterbinary.c
@@ -65,7 +65,7 @@ int main(){
 	//initalization arrays, suppose if there is no value then with -300.99
 	//char * path = "/Users/yuanping/Documents/code/xcode/OptimizeMath/OptimizeMath/math.txt";
     //char * path = "/Users/yuanping/Documents/code/xcode/OptimizeMath/OptimizeMath/simplex2.txt";
-     char * path = "/Users/zhangxulong/Downloads/OptimizeMath-master/OptimizeMath/simplex2.txt";
+     char * path = "/Users/zhangxulong/Downloads/OptimizeMath-master/OptimizeMath/binary.txt";
     //char * path = "math.txt";// xcode please with full path
 	initialization(path);
 
@@ -783,16 +783,18 @@ void calculateBinary(double ** matrix, int rows, int columns){
     
     
     int r,n = 0;
+    int condition;
     //stock values : parameters multiply by (variable = 0 or 1);
 
     //get all of values possible
-    
-    for (r = 0; r <= numberOfInequality; r++) {
+    //对于限制条件求值
+    /*
+    for (r = 0; r < numberOfInequality; r++) {
         for (n = 0; n < numberOfVariable; n++) {
             varibale[r][n][0] = 0 * matrix[r][n];
             varibale[r][n][1] = 1 * matrix[r][n];
-//            printf("%f \n",matrix[r][n]);
-//            printf("%f  %f \n",varibale[r][n][0],varibale[r][n][1]);
+            printf("%f \n",matrix[r][n]);
+            printf("%f  %f \n",varibale[r][n][0],varibale[r][n][1]);
         }
     }
     
@@ -800,14 +802,39 @@ void calculateBinary(double ** matrix, int rows, int columns){
         for (n = numberOfVariable; n < columns; n++) {
             varibale[r][n][0] = 0 ;
             varibale[r][n][1] = 0 ;
+            printf("多余的值\n");
+            printf("%f \n",matrix[r][n]);
+            printf("%f  %f \n",varibale[r][n][0],varibale[r][n][1]);
         }
+    }
+    */
+    
+    //对最后一行Max求值
+    r = numberOfInequality;
+    condition = matrix[r][0];
+    
+        for (n = 0; n <= numberOfVariable; n++) {
+            varibale[r][n][0] = 0 * matrix[r][n+1]; //第一列是符号，所以要n+1
+            varibale[r][n][1] = 1 * matrix[r][n+1];
+            printf("%f \n",matrix[r][n]);
+            printf("%f  %f \n",varibale[r][n][0],varibale[r][n][1]);
+        }
+    for (n = numberOfVariable+1; n < columns; n++) {
+        varibale[r][n][0] = 0 ;
+        varibale[r][n][1] = 0 ;
+        printf("多余的值\n");
+        printf("%f \n",matrix[r][n]);
+        printf("%f  %f \n",varibale[r][n][0],varibale[r][n][1]);
     }
     
     
     
-    int i = 0,j,m=0,s;
+    
+    
+    int i = 0,j,m=0,s,v;
     int x[11]={0};
-    double sum[2<<10] = {0};
+    double sum[2<<10][11] = {0};
+    int var[2<<10][10] = {0};
    
 
 
@@ -843,12 +870,18 @@ void calculateBinary(double ** matrix, int rows, int columns){
     
     
 
+    for (i=0; i<10; i++) {
+        for (j=0; j<2; j++) {
+            printf("varibale[%d][%d][%d] =  %f\n",r,i,j,varibale[r][i][j]);
+        }
+    }
     
 
 
  
 // sum of each line equality
 //多层求和有问题
+    i=0;
     for (x[0]=0; x[0]<=1; x[0]++) {
         for (x[1]=0; x[1]<=1; x[1]++) {
             for (x[2]=0; x[2]<=1; x[2]++) {
@@ -859,11 +892,17 @@ void calculateBinary(double ** matrix, int rows, int columns){
                                 for (x[7]=0; x[7]<=1; x[7]++) {
                                     for (x[8]=0; x[8]<=1; x[8]++) {
                                         for (x[9]=0; x[9]<=1; x[9]++,i++) {
-                                            for(j=0; j<numberOfVariable; j++){
-                                            
-                                            sum[i] = sum[i] + varibale[0][j][x[j]];
-                                                
+                                            sum[i][0] = varibale[r][0][x[0]] + varibale[r][1][x[1]] + varibale[r][2][x[2]]+ varibale[r][3][x[3]] + varibale[r][4][x[4]] + varibale[r][5][x[5]] + varibale[r][6][x[6]] + varibale[r][7][x[7]] + varibale[r][8][x[8]] + varibale[r][9][x[9]] ;
+                                            for (v=1; v<11; v++) {
+                                                sum[i][v] = x[v];
+                                                printf("sum[%d][x%d] = %f, ",i,v,sum[i][v]);
                                             }
+                                            printf("\n");
+                                            
+                                            
+//                                            printf("x[1]= %d, x[2]=%d ,x[3]= %d, x[4]= %d ,x[5]= %d ,x[6]= %d ,x[7]= %d ,x[8]= %d ,x[9]= %d\n",x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9]);
+//                                            printf("varibale[0][0][%d]= %f, varibale[0][1][%d]= %f, varibale[0][2][%d]=%f, varibale[0][3][%d]= %f, varibale[0][4][%d]= %f, varibale[0][5][%d]= %f, varibale[0][6][%d]= %f, varibale[0][7][%d]= %f, varibale[0][8][%d]= %f, varibale[0][9][%d]= %f\n",x[0],varibale[r][0][x[0]],x[1],varibale[r][1][x[1]],x[2],varibale[r][2][x[2]],x[3],varibale[r][3][x[3]],x[4],varibale[r][4][x[4]],x[5],varibale[r][5][x[5]],x[6],varibale[r][6][x[6]],x[7],varibale[r][7][x[7]],x[8],varibale[r][8][x[8]],x[9],varibale[r][9][x[9]]);
+//                                            printf("sum[%d] = %f\n\n",i,sum[i]);
                                             
                                             
                                         }
@@ -885,8 +924,11 @@ void calculateBinary(double ** matrix, int rows, int columns){
         }
     }
     
-
-    
+//    for(j=0; j<numberOfVariable; j++){
+//        
+//        sum[i] = sum[i] + varibale[0][j][x[j]];
+//        
+//    }
     
     
 //                                        sum[i] = varibale[0][0][x[0]] + varibale[0][1][x[1]] + varibale[0][2][x[2]]+ varibale[0][3][x[3]] + varibale[0][4][x[4]] + varibale[0][5][x[5]] + varibale[0][6][x[6]] + varibale[0][7][x[7]] + varibale[0][8][x[8]] + varibale[0][9][x[9]] ;
@@ -894,15 +936,85 @@ void calculateBinary(double ** matrix, int rows, int columns){
    
 //     sum[i] = varibale[0][0][x1] + varibale[0][0][x2] + varibale[0][0][x3]+ varibale[0][0][x4] + varibale[0][0][x5] + varibale[0][0][x6] + varibale[0][0][x7] + varibale[0][0][x8] + varibale[0][0][x9] + varibale[0][0][x10] ;
     
-
     
     
+    //print the result
+    int  numberOfSum = (2<<numberOfVariable);
+    int pp;
+    int k=0,k1=0;
+    int temp[11];
+    
+    pp = pow(2,10-numberOfVariable);
+    
+    //Max[0] ＝ sum，Max[1 to 9] = x1,x2,x3 ...x10;
+    double Max[11],Min[11];
+    Max[0]=sum[0][0];
+    Min[0]=sum[0][0];
+    
+    //double result[2<<10] = {0};
     
     
-    int k=0;
-    for (k=0; k < pow(2,numberOfVariable); k++) {
-        printf("%f\n",sum[k]);
+    //从2^10个结果中选出需要的
+    for (k=0; k <numberOfSum; k++,k1++) {
+        //result[k1] = sum[k*pp][0];
+        printf("%f\n",sum[k*pp][0]);
     }
+    
+    
+    // sort the value of Sum
+    for (i=0; i<(2<<9); i++) {
+        for (j=i+1; j<(2<<9); j++) {
+            if(sum[i][0] < sum[j][0]){
+                for (k=0; k<11; k++) {
+                    temp[k] = sum[i][k];
+                    sum[i][k] = sum[j][k];
+                    sum[j][k] = temp[k];
+                }
+                
+                
+            }
+        }
+    }
+    
+    //print
+   // printf("%d",(2<<9));
+    for (i=0; i<(2<<9); i++) {
+        for (j=0; j<=numberOfVariable; j++) {
+            
+            printf("sum[%d][x%d] = %f, ",i,j,sum[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+    
+    //get the Max and parameters
+//    Max[0] = sum[0][0];
+//    for (k=1; k<11; k++) {
+//        Min[k] = sum[0][k];
+//    }
+ 
+    
+    //        if (Max[0] < sum[i][0]) {
+    //            Max[0] = sum[i][0];
+                for (k=0; k<11; k++) {
+                    Max[k] = sum[0][k];
+                }
+    
+//        if (Min[0] > sum[i][0]) {
+//            Min[0] = sum[i][0];
+//            for (k=1; k<11; k++) {
+//                Min[k] = sum[i][k];
+//            }
+//        }
+//    }
+    
+    printf("Max = %f\n",Max[0] );
+    for (i=1; i<=numberOfVariable; i++) {
+        printf("x%d=%f, ",i,Max[i]);
+    }
+    printf("\n");
+    
     
     //calculate
     //getVaribale_01(varibale0);
